011d734d8b48727bd43e3a963096701c6a45cba1
DrkErk <65326675+DrkErk@users.noreply.github.com>
2023-06-17 23:48:12 -0400

    added ManningPracOfAPyPro file

---
+++ ManningPracOfAPyPro.py
@@ -0,0 +1,27 @@
+import re
+
+# Thoughts on thinking: going over who might be using your software allows you to 
+# to identify the qualitites of the software you want to build out.
+# (with common aspects like, speed, integrity, resources, security/ emphasis on CIA Triad probably)
+# and these can bring out a common outcome like:
+# -Loose Coupling: components aren't intricately dependant on one another
+# -Intuitability: Developers can develop the nature of the sodtware and how it works by reading it
+# -Flexibility: How well developers can adapt the software to relate or similiar tasks
+# -Extensibility: Devs can add/change one aspect of the software without affecting other aspects
+
+# Refactoring: the proccess of updating code to reflect your latest/best practices. Difficulty lies in the when should you
+# (a good start on the path to making a workflow more robust is to split it's logical steps up) 
+# (If each step is handled by it's own service, the service for a particular step only needs to concern...
+# itself with one job.)
+# ---KISS... break down systems into simplier ones
+
+
+
+
+
+
+
+
+
+
+
