0ce61db5b6558c1c021631f528fdff220dfe2dc7
ED <erikdrk@gmail.com>
2023-11-23 16:17:13 -0500

    PracOfAPyPro ch10 feature envy

--- ManningPracOfAPyPro.py
+++ ManningPracOfAPyPro.py
@@ -3035,6 +3035,13 @@ if __name__ == '__main__':
 ######    Feature Envy
 #########
 '''
+-Feature Envy: (When the code performs several tasks using mainly features from another area) (main procedure wants to be the search
+module because it uses all of its features explictly)
+
+-One way feature envy can be fixed: roll it up into a sengle entry point back at the source. 
+(ie when clean_query function was built, the search module is where query cleaning logic goes, so clean query went there.
+the rest of the code is now unaware once we moved it with no more feature envy)
+
 
 
 '''
